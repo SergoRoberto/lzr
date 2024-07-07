124f274fa8bb3cdc7c63d13f096debee359c449d
Сергей <sergey.rudakov@cyberok.ru>
2024-07-07 13:51:22 +0300

    hex text

--- handshakes/mikrotik_api/handshake.go
+++ handshakes/mikrotik_api/handshake.go
@@ -1,6 +1,9 @@
 package mikrotik_api
 
 import (
+	"encoding/hex"
+	"strings"
+
 	"github.com/stanford-esrg/lzr"
 )
 
@@ -9,14 +12,20 @@ type HandshakeMod struct {
 }
 
 func (h *HandshakeMod) GetData(dst string) []byte {
-	data := []byte("") // just wait for banner
-	return data
+	//data := []byte("") // just wait for banner
+	reqHex := "3a0000002f00000002000040020f0001003d050000000000000000000000002f000000000000000000401f0000000000000000000000000000000000"
+	decoded, err := hex.DecodeString(reqHex)
+	if err != nil {
+		return []byte("")
+	}
+
+	return decoded
 }
 
 func (h *HandshakeMod) Verify(data string) string {
-	/* 	if bytes.Equal([]byte(data), []byte("\x01\x00\x00\x00")) {
+	if strings.Contains(hex.EncodeToString([]byte(data)), "0621666174616c0d6e6f74206c6f6767656420696e00") {
 		return "mikrotik_api"
-	} */
+	}
 	return ""
 }
 
