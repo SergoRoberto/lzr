fe901dde64ece3740de2e3249fe2b3f5218f53bc
Sergo Roberto <rudakov.private.bsf@gmail.com>
2024-07-17 12:40:01 +0300

    pov

--- handshakes/mikrotik_wb/handshake.go
+++ handshakes/mikrotik_wb/handshake.go
@@ -22,8 +22,8 @@ func (h *HandshakeMod) GetData(dst string) []byte {
 }
 
 func (h *HandshakeMod) Verify(data string) string {
-	bytesData := []byte(data)
-	if strings.Contains(hex.EncodeToString(bytesData), "026c69737400000000000000010080") || strings.Contains(hex.EncodeToString(bytesData), "026c697374000000000000000200800000000002") {
+	bytesData := hex.EncodeToString([]byte(data))
+	if strings.Contains(bytesData, "026c69737400000000000000010080") || strings.Contains(bytesData, "026c697374000000000000000200800000000002") {
 		return "mikrotik_wb"
 	}
 	return ""
