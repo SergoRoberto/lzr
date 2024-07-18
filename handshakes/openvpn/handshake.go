77163b7318192a73a4afd9cdbddb71f5abb64768
Sergo Roberto <rudakov.private.bsf@gmail.com>
2024-07-18 10:18:43 +0300

    test detect

--- handshakes/openvpn/handshake.go
+++ handshakes/openvpn/handshake.go
@@ -8,11 +8,35 @@ type HandshakeMod struct {
 }
 
 func (h *HandshakeMod) GetData(dst string) []byte {
-
+	return []byte{
+		0x16,       // Тип сообщения: Handshake
+		0x03, 0x01, // Версия: TLS 1.0
+		0x00, 0x2d, // Длина: 45 байт
+		0x01,             // Тип handshake сообщения: ClientHello
+		0x00, 0x00, 0x29, // Длина сообщения: 41 байт
+		0x03, 0x01, // Версия: TLS 1.0
+		0x00, 0x00, 0x00, 0x00, // Random (частично заполнено для примера)
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00,       // Длина session id
+		0x00, 0x04, // Длина списка шифров
+		0x00, 0x2f, // Шифр: TLS_RSA_WITH_AES_128_CBC_SHA
+		0x01,       // Длина списка compression methods
+		0x00,       // Compression method: null
+		0x00, 0x00, // Длина расширений
+	}
 }
 
 func (h *HandshakeMod) Verify(data string) string {
-
+	bytesData := []byte(data)
+	if bytesData[0] == 0x15 || bytesData[0] == 0x16 {
+		return "openvpn"
+	}
+	return ""
 }
 
 func RegisterHandshake() {
