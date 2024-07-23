c92c4afe11851c1edb66ba8eeac40ec293347155
Sergo Roberto <rudakov.private.bsf@gmail.com>
2024-07-23 22:01:38 +0300

    pref

--- handshakes/socks4/handshake.go
+++ handshakes/socks4/handshake.go
@@ -1,9 +1,7 @@
 package socks4
 
 import (
-	"bytes"
 	"encoding/hex"
-	"fmt"
 	"strings"
 
 	"github.com/stanford-esrg/lzr"
@@ -13,38 +11,18 @@ type HandshakeMod struct {
 }
 
 func (h *HandshakeMod) GetData(dst string) []byte {
-	fmt.Println(dst)
-	buffer := new(bytes.Buffer)
-	buffer.WriteByte(0x04) // Версия SOCKS
-	buffer.WriteByte(0x01) // Команда CONNECT
-	/* ipPort := strings.Split(dst, ":")
 
-	port, err := strconv.Atoi(ipPort[1])
-	if err != nil {
-		panic(err)
+	return []byte{
+		0x04, // Версия SOCKS
+		0x01, // Команда CONNECT
+		0x00,
 	}
-	// Записываем порт назначения
-	if err := binary.Write(buffer, binary.BigEndian, int8(port)); err != nil {
-		return []byte("")
-	}
-
-	// Записываем IP-адрес назначения
-	ip := net.ParseIP(ipPort[0]).To4()
-	if ip == nil {
-		return []byte("")
-	}
-	buffer.Write(ip) */
-
-	// Идентификатор пользователя (пустая строка)
-	buffer.WriteByte(0x00)
-
-	return buffer.Bytes()
 
 }
 
 func (h *HandshakeMod) Verify(data string) string {
 	bytesData := hex.EncodeToString([]byte(data))
-	if strings.HasPrefix(bytesData, "005a") || strings.HasPrefix(bytesData, "005b") || strings.HasPrefix(bytesData, "005c") || strings.HasPrefix(bytesData, "005d") {
+	if strings.HasPrefix(bytesData, "005") {
 		return "socks4"
 	}
 	return ""
