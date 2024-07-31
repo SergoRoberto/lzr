8a129653db7e0fd6a913049dbcc239b46201f347
Сергей <sergey.rudakov@cyberok.ru>
2024-07-31 21:27:28 +0300

    invite

--- handshakes/sip/handshake.go
+++ handshakes/sip/handshake.go
@@ -1,6 +1,7 @@
 package sip
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/stanford-esrg/lzr"
@@ -12,19 +13,19 @@ type HandshakeMod struct {
 
 func (h *HandshakeMod) GetData(dst string) []byte {
 
-	/* return []byte(fmt.Sprintf(
-	"INVITE sip:%s SIP/2.0\r\n"+
-		"Via: SIP/2.0/UDP client.atlanta.com;branch=z9hG4bK776asdhds\r\n"+
-		"Max-Forwards: 70\r\n"+
-		"To: <sip:%s>\r\n"+
-		"From: <sip:client@atlanta.com>;tag=1928301774\r\n"+
-		"Call-ID: a84b4c76e66710\r\n"+
-		"CSeq: 4711 INVITE\r\n"+
-		"Contact: <sip:client@client.atlanta.com>\r\n"+
-		"Content-Type: application/sdp\r\n"+
-		"Content-Length: 0\r\n\r\n",
-	dst, dst)) */
-	return []byte(
+	return []byte(fmt.Sprintf(
+		"INVITE sip:%s SIP/2.0\r\n"+
+			"Via: SIP/2.0/UDP client.atlanta.com;branch=z9hG4bK776asdhds\r\n"+
+			"Max-Forwards: 70\r\n"+
+			"To: <sip:%s>\r\n"+
+			"From: <sip:client@atlanta.com>;tag=1928301774\r\n"+
+			"Call-ID: a84b4c76e66710\r\n"+
+			"CSeq: 4711 INVITE\r\n"+
+			"Contact: <sip:client@client.atlanta.com>\r\n"+
+			"Content-Type: application/sdp\r\n"+
+			"Content-Length: 0\r\n\r\n",
+		dst, dst))
+	/* return []byte(
 		"OPTIONS sip:nm@nm SIP/2.0\r\n" +
 			"Via: SIP/2.0/UDP nm;branch=z9hG4bK776asdhds\r\n" +
 			"Max-Forwards: 70\r\n" +
@@ -34,7 +35,7 @@ func (h *HandshakeMod) GetData(dst string) []byte {
 			"CSeq: 4711 OPTIONS\r\n" +
 			"Accept: application/sdp\r\n" +
 			"Content-Length: 0\r\n\r\n",
-	)
+	) */
 }
 
 func (h *HandshakeMod) Verify(data string) string {
